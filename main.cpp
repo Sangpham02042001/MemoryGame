77d9ab2b87253e9c2dc0ca80da89b6645d0535ab
Sangpham02042001 <65053033+Sangpham02042001@users.noreply.github.com>
2020-05-21 22:57:50 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,67 +4,20 @@
 #include <cstdlib>
 #include <SDL.h>
 #include <SDL_image.h>
+
+#include "memory.h"
+
 using namespace std;
 
 //--------------------------------------------------//
-const int MEMORY_CELL_SIZE = 50;   // kích thước ảnh gốc
-const int MEMORY_CELL_TIME = 20;   //
-const int WINDOW_CELL_SIZE = 50;   // kích thước khi hiển thị của một ô
-const int DEFAULT_NUM_ROWS = 6;    // Số lượng ô theo chiều ngang
-const int DEFAULT_NUM_COLS = 6;    // Số lượng ô theo chiều dọc
 
-const string SCREEN_TITLE = "MEMORY";   // Tiêu đề cửa sổ
-const string MEMORY_PATH = "image.jpg";     // Đường dẫn đến file ảnh
-const string LOST = "lost.jpg";
-const string WIN = "win.jpg";
-//-------------------------------------------------//
-
-int Pos[36], Time = -1, click = 0, NowAction[36], PreviousValue = 0, Opened[36], dem = 0;
-
-struct Graphic
-{
-    SDL_Window* window;                 // Cửa sổ hiển thị
-    SDL_Texture* MemoryTexture;         // Ảnh load được từ file ảnh
-    vector<SDL_Rect> MemoryRects;       // Vị trí các hình trong file ảnh
-    SDL_Renderer* renderer;             // Dùng để vẽ hình vào cửa sổ
-};
-
-//------------------------------------------------------//
-
-// Khởi tạo SDL, SDL_Image, tạo cửa sổ, load ảnh, ...  Trả về false nếu khởi
-// tạo không thành công
-bool initGraphic(Graphic& g, int nRows, int nCols);
-
-// Huỷ khởi tạo những gì đã dùng từ SDL và SDL_Image
-void finalizeGraphic(Graphic& g);
-
-// Load ảnh từ đường dẫn path, trả về ảnh biểu diễn dưới dạng SDL_Texture
-SDL_Texture* createTexture(SDL_Renderer* renderer, const string& path);
-
-// Lưu vị trí các hình trong ảnh vào tham số rects
-void initMemoryRects(vector<SDL_Rect>& rects);
-
-// random mảng Pos[36] có giá trị các phần tử từ 0 đến 35
-void random(int nRows, int nCols);
-
-// tạo trạng thái ban đầu cho game
-void initGame(Graphic& g, int nRows, int nCols);
-
-// cập nhât trạng thái cho game
-void update(Graphic& g, int time, int NumberOfClick, int value , int PreviousValue);
-
-// sự kiện bấm chuột
-void ClickAction(SDL_Event& event, Graphic& raphic);
-
-// Hiển thị cửa sổ thông báo lỗi
-void err(const string& msg);
 
 
 int main(int argc, char* argv[])
 {
-    srand(time(0)); // Khởi tạo seed cho hàm rand
-    int nRows = DEFAULT_NUM_ROWS, //6
-        nCols = DEFAULT_NUM_COLS; //6
+    srand(time(0)); // Khoi tao seed cho ham rand
+    int nRows = DEFAULT_NUM_ROWS,
+        nCols = DEFAULT_NUM_COLS;
     Graphic graphic;
     if (!initGraphic(graphic, nRows, nCols)) {
         finalizeGraphic(graphic);
@@ -78,7 +31,7 @@ int main(int argc, char* argv[])
         update(graphic, ++Time, 0, 0, 0);
         while (SDL_PollEvent(&event) != 0)
         {
-            if (event.type == SDL_MOUSEBUTTONDOWN) {    // Sự kiện click chuột
+            if (event.type == SDL_MOUSEBUTTONDOWN) {
                 ClickAction(event, graphic);
             }
         }
@@ -103,29 +56,30 @@ int main(int argc, char* argv[])
     return EXIT_SUCCESS;
 }
 
+
 bool initGraphic(Graphic& g, int nRows, int nCols)
 {
     g.window = NULL;
     g.renderer = NULL;
     g.MemoryTexture = NULL;
-    int sdlFlags = SDL_INIT_VIDEO;  // khởi tạo hệ thống con sự kiện
-    if (SDL_Init(sdlFlags) != 0) // khởi tạo thư viện SDL. trả về 0 khi thành công
+    int sdlFlags = SDL_INIT_VIDEO;  // khoi tao he thong con su kien
+    if (SDL_Init(sdlFlags) != 0) // khoi tao thu vien SDL, tra ve 0 neu thanh cong
     {
         err("SDL could not initialize!");
         return false;
     }
-    int imgFlags = IMG_INIT_PNG;    // Dùng SDL_Image để load ảnh png
+    int imgFlags = IMG_INIT_PNG;    // dung SDL_image de load anh
     if (!(IMG_Init(imgFlags) & imgFlags))
     {
         err("SDL_Image could not initialize!");
         return false;
     }
-    g.window = SDL_CreateWindow(SCREEN_TITLE.c_str(),       // Tiêu đề MEMORY
+    g.window = SDL_CreateWindow(SCREEN_TITLE.c_str(),       // tieu de Memory
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        nCols * WINDOW_CELL_SIZE,  // Chiều rộng
-        nRows * WINDOW_CELL_SIZE + MEMORY_CELL_TIME, // Chiều cao
-        SDL_WINDOW_SHOWN);          // Hiển thị
+        nCols * WINDOW_CELL_SIZE,
+        nRows * WINDOW_CELL_SIZE + MEMORY_CELL_TIME,
+        SDL_WINDOW_SHOWN);
 
     if (g.window == NULL)
     {
@@ -174,29 +128,29 @@ SDL_Texture* createTexture(SDL_Renderer* renderer, const string& path)
 
 void initMemoryRects(vector<SDL_Rect>& rects)
 {
-    SDL_Rect rt;
+    SDL_Rect rect;
     for (long i = 1; i <= 2; ++i)
     {
         for (long j = 0; j < 5; ++j)
         {
-            rt = { j * MEMORY_CELL_SIZE,(i == 1 ? MEMORY_CELL_TIME : MEMORY_CELL_TIME + MEMORY_CELL_SIZE),MEMORY_CELL_SIZE,MEMORY_CELL_SIZE };
-            rects.push_back(rt);
+            rect = { j * MEMORY_CELL_SIZE,(i == 1 ? MEMORY_CELL_TIME : MEMORY_CELL_TIME + MEMORY_CELL_SIZE),MEMORY_CELL_SIZE,MEMORY_CELL_SIZE };
+            rects.push_back(rect);
         }
     }
-    rt = { 0,0,1,MEMORY_CELL_TIME };        // thanh đỏ: thời gian còn lại
-    rects.push_back(rt);
-    rt = { 150,0,1,MEMORY_CELL_TIME };      //thanh trắng : thời gian đã trôi qua
-    rects.push_back(rt);
-    rt = { 0,MEMORY_CELL_TIME + 2 * MEMORY_CELL_SIZE,MEMORY_CELL_SIZE,MEMORY_CELL_SIZE }; // ô đen : đã mở đc 2 ô giống nhau
-    rects.push_back(rt);
-    // rects có 9 ảnh Pokemon + mặt cười + thanh đỏ + thanh trắng + ô đen
+    rect = { 0,0,1,MEMORY_CELL_TIME };        // thanh do : thoi gian con lai
+    rects.push_back(rect);
+    rect = { 150,0,1,MEMORY_CELL_TIME };      // thanh trang : thoi gian da troi qua
+    rects.push_back(rect);
+    rect = { 0,MEMORY_CELL_TIME + 2 * MEMORY_CELL_SIZE,MEMORY_CELL_SIZE,MEMORY_CELL_SIZE }; // o den : mo dc 2 o giong nhau
+    rects.push_back(rect);
+    // rect co 9 anh Pokemon + mat cuoi + thanh do + thanh trang + o den
 }
 
 
 void random(int nRows, int nCols)
-{   // mảng Pos đánh dấu các ô đã dùng
-    // mảng arr lưu vị trí
-    int valmax = nRows * nCols; // 6*6 = 36
+{   // mang Pos danh dau cac o da dung
+    // mang arr luu vi tri
+    int valmax = nRows * nCols;
     int arr[valmax];
     for (int i = 0; i < valmax; ++i)
     {
@@ -218,49 +172,49 @@ void random(int nRows, int nCols)
     {
         Pos[arr[i]] = i;
     }
-    // kết thúc hàm, mảng vt có giá trị các phần tử từ 0 đến 35 và không có giá trị nào bị lặp lại
+    // ket thuc ham, mang Pos co gia tri cac phan tu tu 0 den 35( random vi tri) vaf k co gia tri bi lap lai
 }
 
 
 void initGame(Graphic& g, int nRows, int nCols)
 {
-    SDL_RenderClear(g.renderer);      // Xoá những gì đã vẽ
-    SDL_Rect rt;
-    for (int i = 0; i < nCols * MEMORY_CELL_SIZE; ++i)  // lấy ảnh thanh đỏ thời gian
+    SDL_RenderClear(g.renderer);      // Xoa nhung gi da ve
+    SDL_Rect rect;
+    for (int i = 0; i < nCols * MEMORY_CELL_SIZE; ++i)  // lay thanh do thoi gian
     {
-        rt = { i,0,1,MEMORY_CELL_TIME };            // vị trí hiện time trên của sổ
+        rect = { i,0,1,MEMORY_CELL_TIME };            // vị trí hiện time trên của sổ
         SDL_RenderCopy(g.renderer, g.MemoryTexture,
                         &g.MemoryRects[10] ,     // 10 là vị trí thanh đỏ trong MemoryRect
-                         &rt);
+                         &rect);
     }
     for (int i = 1; i <= nRows; ++i)
     {
         for (int j = 1; j <= nCols; ++j)
         {
-            int val = (i - 1) * nRows + j - 1; // val từ 0 đến 35.
-            rt = { (j - 1) * MEMORY_CELL_SIZE,MEMORY_CELL_TIME + (i - 1) * MEMORY_CELL_SIZE,MEMORY_CELL_SIZE,MEMORY_CELL_SIZE };
+            int val = (i - 1) * nRows + j - 1; // val tu 0 den 35
+            rect = { (j - 1) * MEMORY_CELL_SIZE,MEMORY_CELL_TIME + (i - 1) * MEMORY_CELL_SIZE,MEMORY_CELL_SIZE,MEMORY_CELL_SIZE };
             SDL_RenderCopy(g.renderer, g.MemoryTexture,
-                            &g.MemoryRects[Pos[val] / 4],  // có 9 hình ảnh Pokemon
-                            &rt);
+                            &g.MemoryRects[Pos[val] / 4],  // co 9 hinh anh Pokemon
+                            &rect);
         }
     }
-    SDL_RenderPresent(g.renderer);    // Hiển thị những gì vữa vẽ
-    SDL_Delay(2000); // hiển thị tất cả trong 2s để ghi nhớ
+    SDL_RenderPresent(g.renderer);    // Hien thi nhung gi da ve
+    SDL_Delay(2000); // Hien thi tat ca cac anh trong 2s de ghi nho
 }
 
 
 void update(Graphic& g, int time, int NumberOfClick, int value, int Pre)
 {
-    SDL_RenderClear(g.renderer);      // Xoá những gì đã vẽ
-    SDL_Rect rt;
+    SDL_RenderClear(g.renderer);      // Xoa nhung gi da ve
+    SDL_Rect rect;
     int nRows = DEFAULT_NUM_ROWS,
         nCols = DEFAULT_NUM_COLS;
-    for (int i = 0; i < nCols * MEMORY_CELL_SIZE; ++i)  // lấy ảnh thanh đỏ thời gian
+    for (int i = 0; i < nCols * MEMORY_CELL_SIZE; ++i)
     {
-        rt = { i,0,1,MEMORY_CELL_TIME };            // vị trí hiện time trên của sổ
+        rect = { i,0,1,MEMORY_CELL_TIME };
         SDL_RenderCopy(g.renderer, g.MemoryTexture,
-            &g.MemoryRects[i >= time ? 10 : 11]      // 10 là vị trí thanh đỏ trong MemoryRect và 11 là vị trí thanh trắng trong MemoryRect
-            , &rt);
+            &g.MemoryRects[i >= time ? 10 : 11]      // 10 la vi tri thanh do trong MemoryRect và 11 la vi tri thanh trang trong MemoryRect
+            , &rect);
     }
     for (int i = 1; i <= nRows; ++i)
     {
@@ -270,10 +224,10 @@ void update(Graphic& g, int time, int NumberOfClick, int value, int Pre)
             if (( NumberOfClick == 1 ||  NumberOfClick == 2) && val == value) k = 1; // click lần 1 => hiện và 2 giống nhau sẽ hiện cả 2
             if ( NumberOfClick == 2 && val == Pre ) k = 1; // click lần 2 => hiện
             if (Opened[val] != 0) k = 2;
-            rt = { (j - 1) * MEMORY_CELL_SIZE,MEMORY_CELL_TIME + (i - 1) * MEMORY_CELL_SIZE,MEMORY_CELL_SIZE,MEMORY_CELL_SIZE };
+            rect = { (j - 1) * MEMORY_CELL_SIZE,MEMORY_CELL_TIME + (i - 1) * MEMORY_CELL_SIZE,MEMORY_CELL_SIZE,MEMORY_CELL_SIZE };
             SDL_RenderCopy(g.renderer, g.MemoryTexture,
                            &g.MemoryRects[k == 2 ? 12 : k == 1 ? Pos[val] / 4 : 9] , // 12 là vị trí ô đen trong MemoryRect , 9 là hình mặt cười
-                            &rt);
+                            &rect);
         }
     }
     SDL_RenderPresent(g.renderer);    // Hiển thị những gì vữa vẽ
@@ -327,6 +281,11 @@ void err(const string& m)
 {
     SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", m.c_str(), NULL);
 }
+
+
+
+
+
 
 
 
