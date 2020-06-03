cb7f8a22433b3e34049d917ae548428b38545476
Sangpham02042001 <65053033+Sangpham02042001@users.noreply.github.com>
2020-06-03 22:25:32 +0700

    Update memory.h

--- memory.h
+++ memory.h
@@ -18,12 +18,11 @@ const int DEFAULT_NUM_ROWS = 6;    // so luong o theo chieu ngang
 const int DEFAULT_NUM_COLS = 6;    // so luong o theo chieu doc
 
 const string SCREEN_TITLE = "MEMORY";   // Tieu de cua so
-const string MEMORY_PATH = "image.jpg";     // duong dan den file anh
-const string LOST = "lost.jpg";
+const string MEMORY_PATH = "image.jpg";
 const string WIN = "win.jpg";
+const string LOST = "lost.jpg";
 Mix_Chunk* mix_click;
 Mix_Chunk* mix_game;
-Mix_Chunk* mix_win;
 //-------------------------------------------------//
 
 int Pos[36], Time = -1, click = 0, NowAction[36], PreviousValue = 0, Opened[36], dem = 0;
@@ -38,32 +37,32 @@ struct Graphic
 
 //------------------------------------------------------//
 
-// Khoi tao SDL, SDL_Image, tao cua so, load anh, ...  tra ve false neu khoi
-// tao khong thanh cong
+
+
 bool initGraphic(Graphic& g, int nRows, int nCols);
 
-// Huy khoi tao
+
 void finalizeGraphic(Graphic& g);
 
-// Load anh tu duong dan path, tra ve anh bieu dien duoi dangSDL_Texture
+
 SDL_Texture* createTexture(SDL_Renderer* renderer, const string& path);
 
-// Luu vi tri cac hinh trong anh vao tham so rects
+
 void initMemoryRects(vector<SDL_Rect>& rects);
 
-// random mang Pos[35], co gia tri cac phan tu tu 0 den 35
+
 void random(int nRows, int nCols);
 
-// tao trang thai ban dau cho game
+
 void initGame(Graphic& g, int nRows, int nCols);
 
-// cap nhat trang thai cho game
+
 void update(Graphic& g, int time, int NumberOfClick, int value , int PreviousValue);
 
-// su kien bam chuot
+
 void ClickAction(SDL_Event& event, Graphic& raphic);
 
-// hien thi cua so thong bao loi
+
 void err(const string& msg);
 
 #endif // MEMORY_H_INCLUDED
